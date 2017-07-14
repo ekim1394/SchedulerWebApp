4b4b4446d947c90e07913c7803a5e2ffe08d612c
ekim1394 <simplyeugene94@gmail.com>
2017-07-13 21:39:03 -0400

    fixing project by importing views

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 
-from app import models
+from app import views, models
--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
-SQLALCHEMY_DATABASE_URI  = "192.168.99.100:32768"
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+# SQLALCHEMY_DATABASE_URI  = "192.168.99.100:32768"
+# SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
