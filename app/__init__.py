d0483816fbeb4b56ae965ef81d30bc152ba6be0b
ekim1394 <simplyeugene94@gmail.com>
2017-07-13 21:31:54 -0400

    fixing python project

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
-from app import views
+from app import views, models
